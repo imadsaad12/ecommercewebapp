f281607d5d3cf76854cf45da2fa1e3129130001c
imadsaad <imad.alhaj.saad@gmail.com>
2022-04-28 22:16:17 +0300

    change in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react"
+import axios from "axios"
 
 function App() {
+  useEffect(() => {
+axios.get("http://localhost:4000/home")
+.then(res=>{
+  console.log(res.data)
+})
+    
+  }, []);
+  
   return (
     <div className="App">
       
