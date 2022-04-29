adb85c84d15166900c0a521b9d6693100d865438
Alihashem20 <alih61175@gmail.com>
2022-04-30 00:47:02 +0300

    add miff

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ mongoose
     "mongodb+srv://mydb:93928@cluster0.g0he0.mongodb.net/Ecommerce?retryWrites=true&w=majority"
   )
   .then((res) => {
-    app.listen(4000, () => console.log("server is running on port 4000 . . ."));
+    app.listen(4000, () => { console.log("server is running on port 4000 . . .")
+                             console.log("Welcome To Mazarita International Film Festival")    
+  });
   })
   .catch((err) => {
     console.log(err);
