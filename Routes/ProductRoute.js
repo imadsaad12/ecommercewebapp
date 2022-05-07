aff60abafd20d0452e6c1bd2840c57b3c374a885
ThorTheTroll <hasannawadd@gmail.com>
2022-05-07 13:20:13 +0300

    commited hasan deleteP from test

--- Routes/ProductRoute.js
+++ Routes/ProductRoute.js
@@ -5,5 +5,5 @@ const productController = require("../controllers/ProductController");
 route.post("/", productController.addProduct);
 route.get("/", productController.getProducts);
 route.delete("/:id", productController.deleteProduct);
-
+ 
 module.exports = route
